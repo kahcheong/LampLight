0dd07878059ca46211d77f4cd8cc8bcaaeafbdc7
kahcheong <kc16rr@brocku.ca>
2018-02-01 17:04:21 -0500

    waaaaa & oooof

---
+++ SideScroller/Assets/scripts/BugPath.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugPath : MonoBehaviour {
+
+    public float duration;
+    public string path;
+
+    private bool _wait = true;
+	
+	// Update is called once per frame
+	void Update () {
+	    if (_wait)
+        {
+            iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(path), "time", 5));
+            _wait = false;
+            StartCoroutine(waiting());
+        }	
+
+
+	}
+
+    IEnumerator waiting()
+    {
+        yield return new WaitForSeconds(duration);
+        _wait = true;
+    }
+
+
+}
---
+++ SideScroller/Assets/scripts/displayUI.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class displayUI : MonoBehaviour
+{
+
+    public string myString;
+    public Text myText;
+    public Canvas canvas;
+    public float fadeTime;
+    public bool displayInfo;
+    private Color startColor;
+
+    public bool shouting;
+    public float duration;
+    private bool _wait;
+    public AudioSource audio;
+
+    // Use this for initialization
+    void Start()
+    {
+        startColor = myText.color;
+        myText.color = Color.clear;
+        audio = GetComponent<AudioSource>();
+
+        _wait = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        FadeText();  
+
+        while (shouting && _wait)
+        {
+            _wait = false;
+            StartCoroutine(waiting());
+        }
+
+
+
+    }
+
+    void talk()
+    {
+        displayInfo = true;
+    }
+
+    void OnMouseOver()
+    {
+        displayInfo = true;
+    }
+
+
+    void OnMouseExit()
+
+    {
+        displayInfo = false;
+    }
+
+
+    void FadeText()
+
+    {
+        if (displayInfo)
+        {
+
+            myText.text = myString;
+            myText.color = Color.Lerp(myText.color, startColor, fadeTime * Time.deltaTime);
+        }
+
+        else
+        {
+
+            myText.color = Color.Lerp(myText.color, Color.clear, fadeTime * Time.deltaTime);
+        }
+    }
+
+    IEnumerator waiting()
+    {
+        displayInfo = true;
+        audio.Play();
+        yield return new WaitForSeconds(duration);
+        displayInfo = false;
+        yield return new WaitForSeconds(duration);
+        _wait = true;
+    }
+
+
+}
