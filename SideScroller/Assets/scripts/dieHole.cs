c714716ae185e243ebd2e276e9acf1e496535e27
kahcheong <kc16rr@brocku.ca>
2018-02-14 11:03:38 -0500

    dieHiole complet

---
+++ SideScroller/Assets/scripts/dieHole.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dieHole : MonoBehaviour {
+
+    public GameObject spawnLedge;
+    public GameObject player;
+
+    private void OnCollisionEnter(Collision col)
+    {
+        player.transform.position = spawnLedge.transform.position;
+    }
+
+
+}
