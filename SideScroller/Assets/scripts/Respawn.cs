82eb24b5459a3beb9316bd17b31d297d205cf05a
kahcheong <kc16rr@brocku.ca>
2018-02-14 15:05:18 -0500

    checkpoint work now
    only on first shroom doe
    also not a prefab
    game may broke

---
+++ SideScroller/Assets/scripts/Respawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+
+    public GameObject respawnZone;
+    public GameObject vivi;
+    private Vivi checker;
+
+    private void Start()
+    {
+        vivi = GameObject.Find("vivi 1");
+        checker = vivi.GetComponent<Vivi>();
+    }
+
+    void Update ()
+    {
+
+        if (checker.juice <= 1)
+        {
+            gameObject.transform.position = respawnZone.transform.position;
+            checker.juice = checker.maxJuice;
+        }
+	}
+}
---
+++ SideScroller/Assets/scripts/RespawnZone.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnZone : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject vivi;
+    public float dist;
+    public GameObject juiceSource;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+        vivi = GameObject.Find("vivi 1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        dist = Vector3.Distance(player.transform.position, gameObject.transform.position);
+
+        if (dist < 5f)
+        {
+            juiceSource.SetActive(true);
+            player.GetComponent<Respawn>().respawnZone = gameObject;
+            vivi.GetComponent<Vivi>().recharge = true;
+        }
+        else
+        {
+            vivi.GetComponent<Vivi>().recharge = false;
+        }
+    }
+}
