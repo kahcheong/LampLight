0c25017191cac2253700feccbeab49dfd00df66b
ManPumpkin <DDadkm67$$>
2018-02-17 16:49:22 -0500

    Commenting Code
    Adding comments to code

--- SideScroller/Assets/scripts/displayUI.cs
+++ SideScroller/Assets/scripts/displayUI.cs
@@ -5,17 +5,17 @@ using System.Collections;
 public class displayUI : MonoBehaviour
 {
 
-    public string myString;
-    public Text myText;
-    public Canvas canvas;
-    public float fadeTime;
-    public bool displayInfo;
-    private Color startColor;
+    public string myString;                            //String to display
+    public Text myText;                                //The text of the 
+    public Canvas canvas;                              //The display canvas for the UI
+    public float fadeTime;                             //The time to fade the text in
+    public bool displayInfo;                           //Whether or not to display info
+    private Color startColor;                          //The colour of the text
 
-    public bool shouting;
-    public float duration;
-    private bool _wait;
-    public AudioSource audio;
+    public bool shouting;                              //
+    public float duration;                             //Time to display the text
+    private bool _wait;                                //Whether or not the game is waiting for something
+    public AudioSource audio;                          //Audio to be played
 
     // Use this for initialization
     void Start()
