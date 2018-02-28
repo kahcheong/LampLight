b23865f6a3d2fa46c56a37305b43273cbb9dcbf5
Adam <ah15tp@brocku.ca>
2018-02-27 20:12:16 -0500

    Vivi/Glowfruit
    Added - Vivi
    -Glowfruits
    -New Crystals Models
    -So new enemies and platforms

---
+++ SideScroller/Assets/scripts/GlowFruitPickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowFruitPickup : MonoBehaviour {
+
+
+    public GameObject glowFruit;
+    public Collision trigger;
+    // Use this for initialization
+    void Start () {
+        
+
+	}
+
+    private void OnCollisionEnter(Collision trigger)
+    {
+        gameObject.SetActive(false);
+    }
+}
