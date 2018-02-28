f23c49817019285c79a65fd2d750030fd257b932
ManPumpkin <32469672+ManPumpkin@users.noreply.github.com>
2018-02-28 09:51:03 -0500

    Green/Blue Prefabs
    Yes, we did it boys

---
+++ SideScroller/Assets/scripts/PlatFormPhysicality_PushPull.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatFormPhysicality_PushPull : MonoBehaviour
+{
+    public GameObject[] obstacles;
+    public void OnEnable()
+    {
+        Debug.Log("We inside the cone");
+        for (int i = 0; i < obstacles.Length; i++)
+        {
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            Debug.Log("Enter the cone");
+            other.GetComponentInChildren<PullPlatform>().activated = true;
+        }
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            Debug.Log("Stay in the cone");
+            other.GetComponentInChildren<PullPlatform>().activated = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            Debug.Log("Leave the cone");
+            other.GetComponentInChildren<PullPlatform>().activated = false;
+        }
+    }
+}
