bc253c32aa9da38e0e35cb38c474d4accd40bbef
Adam <ah15tp@brocku.ca>
2018-02-28 08:52:51 -0500

    Glowfruits Work
    Yay

--- SideScroller/Assets/scripts/GlowFruitPickup.cs
+++ SideScroller/Assets/scripts/GlowFruitPickup.cs
@@ -7,14 +7,18 @@ public class GlowFruitPickup : MonoBehaviour {
 
     public GameObject glowFruit;
     public Collision trigger;
+
+    private Vivi vivi;
     // Use this for initialization
+
     void Start () {
-        
+        vivi = GameObject.Find("vivi 1").GetComponent<Vivi>();
 
 	}
 
     private void OnCollisionEnter(Collision trigger)
     {
+        vivi.maxJuice += 25;
         gameObject.SetActive(false);
     }
 }
