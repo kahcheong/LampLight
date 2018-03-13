ff181a48e3d0b891c096c4a60ada18a97f2e7ff6
kahcheong <kc16rr@brocku.ca>
2018-03-12 22:12:28 -0400

    J U I C E

---
+++ SideScroller/Assets/Scripts/JuiceWobble.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JuiceWobble : MonoBehaviour {
+
+    public Sprite[] juices;
+    public int counter = 0;
+    public GameObject self;
+    bool wait = false;
+
+    private void Start()
+    {
+        counter = (int)Random.Range(0, juices.Length);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!wait)
+        {
+            counter = ((counter + 1) % juices.Length);
+            Image temp = self.GetComponent<Image>();
+            temp.sprite = juices[counter];
+            wait = true;
+            StartCoroutine(hwait());
+        }
+    }
+
+    IEnumerator hwait()
+    {
+
+        yield return new WaitForSeconds(0.1f);
+        wait = false;
+    }
+}
+
