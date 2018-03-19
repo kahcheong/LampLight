ccb5a64de7b84b06364f5dd52c2424ee087c53a2
kahcheong <kc16rr@brocku.ca>
2018-03-19 18:16:18 -0400

    moth boys dissolve

---
+++ SideScroller/Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour {
+
+    public GameObject[] GO;
+    public Material[] MR;
+    public bool kill = false;
+
+    // Use this for initialization
+    private void Update()
+    {
+        if (kill) StartCoroutine("Disintegrate");
+    }
+    IEnumerator Disintegrate()
+    {
+        for (float j = 0; j < 1; j += 0.03f)
+        {
+            for (int i = 0; i < GO.Length; i++)
+            {
+                GO[i].GetComponent<Renderer>().material.SetFloat("_SliceAmount", j);
+                GO[i].GetComponent<Renderer>().material.SetFloat("_BurnSize", j);
+            }
+            yield return null;
+            Debug.Log(j);
+        }
+        Destroy(gameObject);
+    }
+}
