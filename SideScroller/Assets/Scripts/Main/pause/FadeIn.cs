730b47f94eaa58d8f12f4a90f55862b3f6c0234e
kahcheong <kc16rr@brocku.ca>
2018-03-27 17:04:07 -0400

    spicy memes

---
+++ SideScroller/Assets/Scripts/Main/pause/FadeIn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FadeIn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Fader(gameObject.GetComponent<Image>()));
+	}
+	
+
+
+    IEnumerator Fader(Image img)
+    {
+        for (float f = img.color.a; f >= 0; f -= 0.05f)
+        {
+            Color c = img.color;
+            c.a = f;
+            img.color = c;
+            yield return null;
+        }
+        Destroy(img.gameObject);
+    }
+}
